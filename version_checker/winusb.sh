d448a10ddd8cbe3bd921abb3e2cef5ab1bb58a60
amazingfate <liujianfeng1994@gmail.com>
2015-08-04 16:40:21 +0800

    fix winusb script

--- version_checker/winusb.sh
+++ version_checker/winusb.sh
@@ -6,7 +6,7 @@ current_version_line=`grep "Version: " current_package_version.txt`
 current_version=`echo ${current_version_line#*Version: }`
 version_line=`curl http://en.congelli.eu/prog_info_winusb.html |grep "winusb/winusb-"|sed 1q`
 version_1=`echo ${version_line#*winusb/winusb-}`
-version=`echo ${version_1%.tar.gz"}`
-echo "$pkgname"
+version=`echo ${version_1%.tar.gz\" type=\"*}`
+echo "package name: $pkgname"
 echo "current version: $current_version"
 echo "upstream version: $version"
